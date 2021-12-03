40adba1007d101006b2426a802ee8638e27e335f
michelltc <mchristanto01@student.ciputra.ac.id>
2021-12-03 10:41:20 +0700

    cek prima atau bukan

--- Prima/Program.cs
+++ Prima/Program.cs
@@ -6,7 +6,18 @@ namespace Prima
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var angka = 0;
+            Console.Write("Masukkan angka : ");
+            angka = Convert.ToInt32(Console.ReadLine());
+          
+                if ((angka % 2 == 1) && (angka / angka == 1))
+                {
+                    Console.WriteLine("Bilangan Prima");
+                }
+                else 
+                {
+                    Console.WriteLine("Bukan Bilangan Prima");
+                }
         }
     }
 }
